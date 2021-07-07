eeb26b1c0007126c1e0d8f15593ce54cbbf0a77c
Marcin Makuch <m.makuch@gidcoders.pl>
2021-07-08 01:02:40 +0200

    Initial commit

---
+++ src/main/java/com/makuch/simplemarkethandler/SimpleMarketHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.makuch.simplemarkethandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMarketHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMarketHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makuch/simplemarkethandler/SimpleMarketHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makuch.simplemarkethandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMarketHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
